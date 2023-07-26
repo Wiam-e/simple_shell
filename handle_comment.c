d34f6efedda3e62a9f8eda00225342a9c4dc3eed
Wiam-e <wiam.errabih@gmail.com>
2023-07-26 13:03:20 -0700

    Betty fix

--- handle_comment.c
+++ handle_comment.c
@@ -1,22 +1,21 @@
 #include "shell.h"
 /**
- * handle_comment - cuts a string when #
- * @s: string to cut
+*handle_comment - cuts a string when #
+* @s: string to cut
  * Return: new string
  */
-
 char *handle_comment(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '#')
-        {
-            s[i] = '\0';
-            break;
-        }
-        i++;
-    }
-    return (s);
+	while (s[i] != '\0')
+	{
+	if (s[i] == '#')
+	{
+		s[i] = '\0';
+		break;
+	}
+	i++;
+	}
+	return (s);
 }
